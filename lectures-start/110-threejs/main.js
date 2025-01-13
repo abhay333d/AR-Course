a09da4095e22fd8ddac10b2b5a91f55cf9090cb8
abhay333d <abhaydandge333@gmail.com>
2025-01-14 01:02:44 +0530

    added required files

---
+++ lectures-start/110-threejs/main.js
@@ -0,0 +1,5 @@
+// import * as THREE from "../../libs/three.js-r132/build/three.module.js"; //this does not work
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.132.0/build/three.module.js";
+
+
+console.log("THREE", THREE);

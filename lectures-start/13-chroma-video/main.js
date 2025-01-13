cf619bec2944e6ba66a3f8d00191c0c33697667d
abhay333d <abhaydandge333@gmail.com>
2025-01-14 01:08:16 +0530

    done with all the setups

--- lectures-start/13-chroma-video/main.js
+++ lectures-start/13-chroma-video/main.js
@@ -1,5 +1,5 @@
 import {loadGLTF, loadVideo} from "../../libs/loader.js";
-import {mockWithVideo} from '../../libs/camera-mock';
+import {mockWithVideo} from '../../libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
